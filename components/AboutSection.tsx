a9577303f90242e9a8d977607534125ff69b8995
Graphfied <mrlegendg007@gmail.com>
2023-05-23 00:35:13 +0500

    Initial commit

---
+++ components/AboutSection.tsx
@@ -0,0 +1,97 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "TypeScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Tailwind CSS" },
+  { skill: "Git" },
+  { skill: "Blender3D,3Dsmax" },
+  { skill: "Unreal Engine" },
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-red-600 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Get to know me!
+            </h1>
+            <p>
+              Hi, my name is Fahad Ghouri and I am a{" "}
+              <span className="font-bold">{"highly ambitious"}</span>,
+              <span className="font-bold">{" self-motivated"}</span>, and
+              <span className="font-bold">{" Selfmade"}</span> Game Developer and 3D modeler
+              / Animator
+            </p>
+            <br />
+            <p>
+            Introducing Fahad Ghouri, a versatile and highly skilled freelancer specializing in game development, 
+            modeling, and animation. With a solid foundation in computer science and a keen eye
+            for aesthetics,I creates immersive virtual worlds with captivating gameplay experiences.
+            </p>
+            <br />
+            <p>
+            I brings a unique combination of technical knowledge and artistic flair to every 
+            project. They excel in creating visually appealing assets, from realistic environments 
+            to stylized characters, while effectively translating concepts into stunning visual representations.
+
+
+            With a proven track record of successful projects and a growing portfolio of impressive work,
+            Im ready to take on new challenges in the game development and animation industry. 
+            Contact Fahadhassanghouri@gmail.com today to experience the transformative power of their talent and 
+            bring your creative visions to reality.
+            </p>
+            <br />
+            <p>
+              I believe that you should{" "}
+              <span className="font-bold text-red-600">
+                never stop growing
+              </span>{" "}
+              and that&#39;s what I strive to do, I have a passion for
+              technology and a desire to always push the limits of what is
+              possible. I am excited to see where my career takes me and am
+              always open to new opportunities. 🙂
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/hero-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
---
+++ components/ProjectsSection.tsx
@@ -0,0 +1,89 @@
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import SlideUp from "./SlideUp"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+  {
+    name: "ARTIC AI",
+    description:
+      "CHAT-GPT BASE CHATBOT",
+    image: "/thankfulthoughts.png",
+    github: "https://github.com/Graphfied/Artic-AI",
+    link: "https://github.com/Graphfied/Artic-AI",
+  },
+  {
+    name: "project PIAIC",
+    description: "PIAIC PROJECTS",
+    image: "/platoio.png",
+    github: "https://github.com/graphfied",
+    link: "https://github.com/graphfied",
+  },
+  {
+    name: "Instagram Profile",
+    description:
+      "you can check my work on my instagram profile",
+    image: "/familyphotos.png",
+    github: "https://www.instagram.com/faddy3d/",
+    link: "https://www.instagram.com/faddy3d/",
+  },
+]
+
+const ProjectsSection = () => {
+  return (
+    <section id="projects">
+      <h1 className="my-10 text-center font-bold text-4xl">
+        Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-red-600 border-0 rounded"></hr>
+      </h1>
+
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, idx) => {
+          return (
+            <div key={idx}>
+              <SlideUp offset="-300px 0px -300px 0px">
+                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className=" md:w-1/2">
+                    <Link href={project.link}>
+                      <Image
+                        src={project.image}
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="rounded-xl shadow-xl hover:opacity-70"
+                      />
+                    </Link>
+                  </div>
+                  <div className="mt-8 md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                      {project.description}
+                    </p>
+                    <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SlideUp>
+            </div>
+          )
+        })}
+        
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsSection
